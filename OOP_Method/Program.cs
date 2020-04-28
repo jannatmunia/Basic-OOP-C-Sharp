6fd8ee0e0ae15e3d2532249f7a7534fbdb56e28c
jannatmunia <43574672+jannatmunia@users.noreply.github.com>
2020-04-28 22:19:47 +0600

    Add files via upload

---
+++ OOP_Method/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.method();
+            Console.ReadKey();
+
+        }
+        /*
+        Access Specifier return type method_name (Parameter List)
+        {
+            Method Body;
+        }
+        */
+        public void method()
+        {
+            Console.WriteLine("This is a method");
+        }
+    }
+}
