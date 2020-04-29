1369940b581cbbdeaca8c375ce72149768023517
jannatmunia <43574672+jannatmunia@users.noreply.github.com>
2020-04-30 01:52:13 +0600

    Add files via upload

---
+++ CallByReferenceMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CallByReferenceMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            Console.WriteLine("Enter a variable:");
+            int a = Convert.ToInt32(Console.ReadLine());
+           
+            Console.WriteLine(obj.reference(ref a));
+            Console.WriteLine("a is {0}",a);
+            Console.ReadKey();
+        }
+        public int reference(ref int z)
+        {
+            return z = 200;
+        }
+    }
+}
