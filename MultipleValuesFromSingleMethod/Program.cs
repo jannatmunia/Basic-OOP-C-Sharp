aba5fddf484b07a3a2296eb70fdd762879c9a6d3
jannatmunia <43574672+jannatmunia@users.noreply.github.com>
2020-04-30 21:32:59 +0600

    Add files via upload

---
+++ MultipleValuesFromSingleMethod/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace MultipleValuesFromSingleMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a no:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a no :");
+            int y = Convert.ToInt32(Console.ReadLine());
+            int sum, sub,mul,div;
+            /*out is a simple keyword
+             */
+            method(x, y,out sum,out sub,out mul,out div);
+            Console.WriteLine("sum,substraction,multiplication,division is : {0},{1},{2},{3}", sum, sub,mul,div);
+            Console.ReadKey();
+        }
+        public static void method(int a,int b,out int sum,out int sub,out int mul,out int div)
+        {
+            sum = a + b;
+            sub = a - b;
+            mul = a * b;
+            div = a / b;
+        }
+       
+    }
+}
