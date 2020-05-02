df9e5c61b4270bf44b69fb5d87d217b79bef9472
jannatmunia <43574672+jannatmunia@users.noreply.github.com>
2020-05-02 12:29:29 +0600

    Add files via upload

---
+++ PassingArrayIn_Method/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassingArrayIn_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = new string[3];
+            data[0] = "CE";
+            data[1] = "SE";
+            data[2] = "CE";
+            method(data);
+            int[] agedata = new int[2];
+            agedata[0] = 20;
+            agedata[1] = 30;
+            age(agedata);
+
+            Console.ReadKey();
+        }
+        public static void method(string[] name)
+        {
+            foreach (string value in name)
+            {
+                Console.WriteLine(value);
+            }
+        }
+        public static void age(int[] age)
+        {
+            foreach(int value in age)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
