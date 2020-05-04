a8a8aac960bda8332b0329cfbcd18162c0280d8c
jannatmunia <43574672+jannatmunia@users.noreply.github.com>
2020-05-04 11:28:49 +0600

    Add files via upload

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(multi(10.10));
+            Console.WriteLine(multi(10));
+            Console.WriteLine(multi("x"));
+            Console.WriteLine(multi('a'));
+            Console.ReadKey();
+        }
+        public static double multi(double x)
+        {
+            return x * x;
+        }
+        public static int multi(int x)
+        {
+            return x * x;
+        }
+        public static string multi(string y)
+        {
+            string st = "hello";
+            return st + y;
+        }
+        public static char multi(char a)
+        {
+            return a;
+        }
+    }
+}
